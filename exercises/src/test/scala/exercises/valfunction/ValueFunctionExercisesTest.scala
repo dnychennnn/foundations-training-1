bdcbbce95abacf1c7b21278bfb58c08c691419c2
ychen <yung-yu.chen@autoscout24.com>
2021-12-07 10:11:18 +0100

    Exercise 1 with PBT tests

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -12,22 +12,44 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   /////////////////////////////////////////////////////
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits examples") {
+  test("selectDigits examples") {
     assert(selectDigits("hello4world-80") == "480")
     assert(selectDigits("welcome") == "")
   }
 
-  ignore("selectDigits length is smaller") {
+  test("selectDigits length is smaller") {
     forAll { (text: String) =>
       assert(selectDigits(text).length <= text.length)
     }
   }
 
-  ignore("secret") {
+  //PBT
+  test("PBT: selectDigits' property is all numerical") {
+    forAll { (text: String) =>
+      assert(selectDigits(text).forall(_.isDigit))
+    }
+  }
+
+  test("secret") {
     assert(secret("abc123") == "******")
   }
 
-  ignore("isValidUsernameCharacter") {
+  //PBT
+  test("PBT: secret length should be the same as input") {
+    forAll { (text: String) =>
+      assert(secret(text).length == text.length)
+    }
+  }
+
+  test("secret could be executed in parallel") {
+    forAll { (text1: String, text2: String) =>
+      val secret1 = secret(text1 + text2)
+      val secret2 = secret(text1) + secret(text2)
+      assert(secret1 == secret2)
+    }
+  }
+
+  test("isValidUsernameCharacter") {
     assert(isValidUsernameCharacter('a'))
     assert(isValidUsernameCharacter('A'))
     assert(isValidUsernameCharacter('1'))
@@ -37,7 +59,7 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
     assert(!isValidUsernameCharacter('!'))
   }
 
-  ignore("isValidUsername") {
+  test("isValidUsername") {
     assert(isValidUsername("john-doe"))
     assert(!isValidUsername("*john*"))
   }
@@ -46,21 +68,34 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   // Exercise 2: Point
   ///////////////////////
 
-  ignore("isPositive") {
+  test("isPositive") {
     assert(Point(2, 4, 9).isPositive)
     assert(Point(0, 0, 0).isPositive)
     assert(!Point(0, -2, 1).isPositive)
   }
 
-  ignore("isEven") {
+  //PBT -> special case -2147483648 if use x.abs
+  test("PBT: isPositive") {
+    forAll(Gen.posNum[Int], Gen.posNum[Int], Gen.posNum[Int]) { (x: Int, y: Int, z: Int) =>
+      assert(Point(x, y, z).isPositive)
+    }
+  }
+
+  test("isEven") {
     assert(Point(2, 4, 8).isEven)
     assert(Point(0, -8, -2).isEven)
     assert(!Point(3, -2, 0).isEven)
   }
 
-  ignore("forAll") {
+  test("forAll") {
     assert(Point(1, 1, 1).forAll(_ == 1))
     assert(!Point(1, 2, 5).forAll(_ == 1))
   }
 
+  //PBT
+  test("PBT: forAll from Point should be consistent to standard lib (e.g. List)") {
+    forAll { (testX: Int, testY: Int, testZ: Int, testR: Int => Boolean) =>
+      assert(List(testX, testY, testZ).forall(testR) == Point(testX, testY, testZ).forAll(testR))
+    }
+  }
 }
